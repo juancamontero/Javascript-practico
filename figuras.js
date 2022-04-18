b3f088886c4a3da7488df33bd9304febff9ab79c
Juan Montero <juanca.montero76@gmail.com>
2022-04-18 11:17:18 -0500

    reto H isoceles

--- figuras.js
+++ figuras.js
@@ -1,21 +1,14 @@
 //CUADRADO
 
-const squareSide = 5;
+
 const squarePerimeter = (side) => 4 * side;
 const squareArea = (side) => side * side;
 
-
-
 //TRIANGULO
-console.group("Triángulo");
-const trianSide1 = 6;
-const trianSide2 = 8;
-const trianBase = 4;
-const trianHeigth = 5.5;
+
 
 const trianPerimeter = (s1, s2, s3) => s1 + s2 + s3;
 const trianArea = (base, heigth) => (base * heigth) / 2;
-
 
 //CIRCULO
 const pi = Math.PI;
@@ -24,9 +17,12 @@ const radius = 4;
 const circlePerimeter = (r) => pi * r * 2;
 const circleArea = (r) => pi * r * r;
 
+// ALTURA ISOCELES
 
+const esIsoceles = (l1, l2, l3) =>
+  l1 === l2 || l1 === l3 || l2 === l3 ? true : false;
 
-
+const heigthTrian = (base, side) => Math.sqrt(side ** 2 - (base / 2) ** 2);
 
 // interactua con HTML
 
@@ -43,34 +39,47 @@ function calcSquareArea() {
 }
 
 function calcTrianPerim() {
-  const l1 = parseInt(document.getElementById("side1").value);
-  const l2 = parseInt(document.getElementById("side2").value);
-  const b = parseInt(document.getElementById("base").value);
+  const l1 = parseFloat(document.getElementById("side1").value);
+  const l2 = parseFloat(document.getElementById("side2").value);
+  const b = parseFloat(document.getElementById("base").value);
 
   const perimeter = trianPerimeter(l1, l2, b);
   alert(perimeter);
 }
 
 function calcTrianArea() {
-  const h = parseInt(document.getElementById("heigth").value);
-  const b = parseInt(document.getElementById("base").value);
+    debugger;
+  const h = parseFloat(document.getElementById("heigth").value);
+  const b = parseFloat(document.getElementById("base").value);
 
   const area = trianArea(b, h);
   alert(area);
 }
 
 function calcCirclePerim() {
-    const r = parseInt(document.getElementById("radius").value);
-   
-  
-    const perimeter = circlePerimeter(r)
-    alert(perimeter);
-  }
+  const r = parseFloat(document.getElementById("radius").value);
 
-  function calcCircleArea() {
-    const r = parseInt(document.getElementById("radius").value);
-   
-  
-    const area = circleArea(r)
-    alert(area);
+  const perimeter = circlePerimeter(r);
+  alert(perimeter);
+}
+
+function calcCircleArea() {
+  const r = parseFloat(document.getElementById("radius").value);
+
+  const area = circleArea(r);
+  alert(area);
+}
+
+function isocelesProcess() {
+debugger;
+
+  const l1 = parseFloat(document.getElementById("side1i").value);
+  const l2 = parseFloat(document.getElementById("side2i").value);
+  const base = parseFloat(document.getElementById("basei").value);
+
+  if (esIsoceles(l1, l2, base)) {
+    alert("Es isóceles y la altura es " + heigthTrian(base, l1) + " cms");
+  } else {
+    alert("No es isóceles");
   }
+}
