659663353ca0075c8d5b86ec8ef733a981aa3ee0
Juan Montero <juanca.montero76@gmail.com>
2022-04-18 10:24:27 -0500

    borrar console log del js

--- figuras.js
+++ figuras.js
@@ -4,13 +4,7 @@ const squareSide = 5;
 const squarePerimeter = (side) => 4 * side;
 const squareArea = (side) => side * side;
 
-console.group("Cuadrado");
-console.log("Lado del cuadrado es -> " + squareSide + " cm");
-console.log(
-  "Perimetro del cuadrado es -> " + squarePerimeter(squareSide) + " cm"
-);
-console.log("Area del cuadrado es -> " + squareArea(squareSide) + " cmˆ2");
-console.groupEnd();
+
 
 //TRIANGULO
 console.group("Triángulo");
@@ -22,28 +16,6 @@ const trianHeigth = 5.5;
 const trianPerimeter = (s1, s2, s3) => s1 + s2 + s3;
 const trianArea = (base, heigth) => (base * heigth) / 2;
 
-console.log(
-  "los lados del trinagulo son: " +
-    trianBase +
-    "cm, " +
-    trianSide1 +
-    "cm, " +
-    trianSide2 +
-    "cm, "
-);
-
-console.log("Altura triángulo-> " + trianHeigth + "cms");
-console.log("Base triángulo-> " + trianBase + "cms");
-console.log(
-  "Perimetro del triángulo es -> " +
-    trianPerimeter(trianBase, trianSide1, trianSide2) +
-    " cm"
-);
-console.log(
-  "Area del triángulo es -> " + trianArea(trianBase, trianHeigth) + " cmˆ2"
-);
-
-console.groupEnd();
 
 //CIRCULO
 const pi = Math.PI;
@@ -52,13 +24,9 @@ const radius = 4;
 const circlePerimeter = (r) => pi * r * 2;
 const circleArea = (r) => pi * r * r;
 
-console.group("Circulo");
 
-console.log("Radio Circulo-> " + radius + "cms");
-console.log("Perimetro del círculo es -> " + circlePerimeter(radius) + " cm");
-console.log("Area del círculo es -> " + circleArea(radius) + " cmˆ2");
 
-console.groupEnd();
+
 
 // interactua con HTML
 
